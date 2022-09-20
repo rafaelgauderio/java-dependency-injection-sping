dac3d5d2913bec712c8e792f8b116c6218616107
Rafael De Luca <deluca1712@gmail.com>
2022-09-20 17:02:42 -0300

    Criando projeto spring web

---
+++ src/test/java/com/rafaeldeluca/injecaodependencia/InjecaoDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.injecaodependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaoDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
